ef73ec877c7e0e9219143459e7995bd4d0ca9f96
KyroKrypt <kyrokrypt@gmail.com>
2021-02-09 22:16:59 -0800

    reworked AOutput to use static methods again

--- src/main/java/dev/kyro/arcticapi/ArcticAPI.java
+++ src/main/java/dev/kyro/arcticapi/ArcticAPI.java
@@ -2,8 +2,22 @@ package dev.kyro.arcticapi;
 
 public class ArcticAPI {
 
-    public ArcticAPI() {
+    public static String prefix;
+    public static String errorPrefix;
 
+    public ArcticAPI(String prefix, String errorPrefix) {
 
+        ArcticAPI.prefix = prefix;
+        ArcticAPI.errorPrefix = errorPrefix;
+    }
+
+    public static String getPrefix() {
+
+        return prefix == null ? "" : prefix;
+    }
+
+    public static String getErrorPrefix() {
+
+        return errorPrefix == null ? "" : errorPrefix;
     }
 }
--- src/main/java/dev/kyro/arcticapi/misc/AOutput.java
+++ src/main/java/dev/kyro/arcticapi/misc/AOutput.java
@@ -1,35 +1,27 @@
 package dev.kyro.arcticapi.misc;
 
+import dev.kyro.arcticapi.ArcticAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class AOutput {
 
-    public String prefix;
-    public String errorPrefix;
+    public static void send(Player player, String message) {
 
-    public AOutput(String prefix, String errorPrefix) {
-
-        this.prefix = prefix;
-        this.errorPrefix = errorPrefix;
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', ArcticAPI.getPrefix() + message));
     }
 
-    public void send(Player player, String message) {
+    public static void error(Player player, String message) {
 
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', ArcticAPI.getErrorPrefix() + message));
     }
 
-    public void error(Player player, String message) {
-
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', errorPrefix + message));
-    }
-
-    public void color(Player player, String message) {
+    public static void color(Player player, String message) {
 
         player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 
-    public void raw(Player player, String message) {
+    public static void raw(Player player, String message) {
 
         player.sendMessage(message);
     }
