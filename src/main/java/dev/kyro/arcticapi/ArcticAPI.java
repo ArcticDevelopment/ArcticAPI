678d316d91211b6785e0d75e3cdbb093a77401d9
KyroKrypt <kyrokrypt@gmail.com>
2021-02-10 10:58:02 -0800

    Added way to initialize API with prefixes from config

--- src/main/java/dev/kyro/arcticapi/ArcticAPI.java
+++ src/main/java/dev/kyro/arcticapi/ArcticAPI.java
@@ -1,5 +1,6 @@
 package dev.kyro.arcticapi;
 
+import dev.kyro.arcticapi.data.AConfig;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ArcticAPI {
@@ -15,5 +16,13 @@ public class ArcticAPI {
 
         ArcticAPI.prefix = prefix;
         ArcticAPI.errorPrefix = errorPrefix;
+    }
+
+    public static void configInit(JavaPlugin plugin, String prefix, String errorPrefix) {
+
+        ArcticAPI.plugin = plugin;
+
+        ArcticAPI.prefix = AConfig.getString(prefix);
+        ArcticAPI.errorPrefix = AConfig.getString(errorPrefix);
     }
 }
