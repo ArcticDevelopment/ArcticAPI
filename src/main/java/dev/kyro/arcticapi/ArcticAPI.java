cbe68ca0f7644f3cfb8c2280fb717031b1cbce99
KyroKrypt <kyrokrypt@gmail.com>
2021-02-09 22:42:50 -0800

    made api fully static added AConfig

--- src/main/java/dev/kyro/arcticapi/ArcticAPI.java
+++ src/main/java/dev/kyro/arcticapi/ArcticAPI.java
@@ -1,13 +1,17 @@
 package dev.kyro.arcticapi;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 public class ArcticAPI {
 
-//    public static JavaPlugin plugin;
+    public static JavaPlugin plugin;
 
     public static String prefix = "";
     public static String errorPrefix = "";
 
-    public ArcticAPI(String prefix, String errorPrefix) {
+    public static void setup(JavaPlugin plugin, String prefix, String errorPrefix) {
+
+        ArcticAPI.plugin = plugin;
 
         ArcticAPI.prefix = prefix;
         ArcticAPI.errorPrefix = errorPrefix;
---
+++ src/main/java/dev/kyro/arcticapi/config/AConfig.java
@@ -0,0 +1,36 @@
+package dev.kyro.arcticapi.config;
+
+import dev.kyro.arcticapi.ArcticAPI;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.List;
+
+public class AConfig {
+
+    public static FileConfiguration config = ArcticAPI.plugin.getConfig();
+
+    public static String getString(String path) {
+
+        return config.getString(path);
+    }
+
+    public static boolean getBoolean(String path) {
+
+        return config.getBoolean(path);
+    }
+
+    public static int getInt(String path) {
+
+        return config.getInt(path);
+    }
+
+    public static double getDouble(String path) {
+
+        return config.getDouble(path);
+    }
+
+    public static List<String> getStringList(String path) {
+
+        return config.getStringList(path);
+    }
+}
