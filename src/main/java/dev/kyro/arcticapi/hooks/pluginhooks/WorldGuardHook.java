b99341f0c456abb293472806fe8ee2bc33cc17f3
KyroKrypt <kyrokrypt@gmail.com>
2021-02-21 22:55:18 -0800

    Minor Fix

--- src/main/java/dev/kyro/arcticapi/hooks/pluginhooks/WorldGuardHook.java
+++ src/main/java/dev/kyro/arcticapi/hooks/pluginhooks/WorldGuardHook.java
@@ -44,8 +44,8 @@ public class WorldGuardHook {
 
 			for(Map.Entry<Flag<?>, Object> flagEntry : protectedRegion.getFlags().entrySet()) {
 
-				System.out.println(flagEntry.getKey().getName().equals(flagName));
-				if(flagEntry.getKey().getName().equals(flagName)) return true;
+				System.out.println(flagEntry.getKey().getName().equals(flagName) && (Boolean) flagEntry.getValue());
+				if(flagEntry.getKey().getName().equals(flagName) && (Boolean) flagEntry.getValue()) return true;
 			}
 		}
 		return false;
