4eb1117cb2242bbb71a3835c35bef6cf1a17b927
KyroKrypt <kyrokrypt@gmail.com>
2021-03-06 16:02:41 -0800

    Added giveItemSafely method

--- src/main/java/dev/kyro/arcticapi/misc/AUtil.java
+++ src/main/java/dev/kyro/arcticapi/misc/AUtil.java
@@ -1,6 +1,12 @@
 package dev.kyro.arcticapi.misc;
 
+import dev.kyro.arcticapi.ArcticAPI;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.Collections;
 import java.util.TreeMap;
@@ -41,6 +47,46 @@ public class AUtil {
 		int full = (int) (length * percentFull);
 		return fullColor.toString() + String.join("", Collections.nCopies(full, tickMark))
 				+ emptyColor.toString() + String.join("", Collections.nCopies(length - full, tickMark));
+	}
+
+	public static void giveItemSafely(Player player, ItemStack itemStack, boolean secondChance) {
+
+		Inventory inventory = player.getInventory();
+
+		if(inventory.firstEmpty() != -1) {
+
+			inventory.addItem(itemStack);
+		}
+
+		AOutput.error(player, "&4&lWARNING!!! YOUR INVENTORY IF FULL." +
+				" Please make space for an item to be added or it will be dropped on the ground in 5 seconds.");
+
+		ASound.play(player, Sound.NOTE_PLING);
+
+		if(!secondChance) {
+
+			AOutput.send(player, "The item has been dropped on the ground at your location");
+			player.getWorld().dropItem(player.getLocation(), itemStack);
+
+			return;
+		}
+
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				Inventory inventory = player.getInventory();
+
+				if(inventory.firstEmpty() != -1) {
+
+					inventory.addItem(itemStack);
+					AOutput.send(player, "The item has been safely added to your inventory");
+				} else {
+
+					AOutput.send(player, "The item has been dropped on the ground at your location");
+					player.getWorld().dropItem(player.getLocation(), itemStack);
+				}
+			}
+		}.runTaskLater(ArcticAPI.PLUGIN, 100L);
 	}
 }
 
